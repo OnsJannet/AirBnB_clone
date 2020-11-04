bf5e050985949b1e5c848988745d92cadcd91588
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-11-04 22:26:49 +0100

    fix pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,31 +10,27 @@ from models.user import User
 import json
 import os
 
-class FileStorage:
 
+class FileStorage:
     """ A class called FileStorage
     attributes:
     attr1(__file_path): JSON file path
     attr2(__objects): dictionary
     """
-
-
     def __init__(self):
         """initializing an instance"""
         self.__file_path = "file.json"
-        open(self.__file_path, "a") 
+        open(self.__file_path, "a")
         self.__objects = {}
 
     def all(self):
         """ return all objects"""
         return self.__objects
-        
 
     def new(self, obj):
         """ sets in __objects the obj with key"""
         key = obj.__class__.__name__ + '.' + obj.id
         self.__objects[key] = obj
-        
 
     def save(self):
         """ save to JSON"""
@@ -43,7 +39,6 @@ class FileStorage:
             dict[key] = value.to_dict()
         with open(self.__file_path, mode="w") as my_file:
             json.dump(dict, my_file)
-
 
     def reload(self):
         """reload from JSON"""
@@ -58,5 +53,4 @@ class FileStorage:
                 dict_to_fill = json.load(json_file)
                 for key, value in dict_to_fill.items():
                     self.__objects[key] = ids[value
-                                  ['__class__']](**value)
-
+                                              ['__class__']](**value)
