10582b57e0187f9866f8dd8a51df63ee1f0f86be
Aymen-haddaji-hub <aymensystem7@gmail.com>
2020-11-05 19:58:17 +0100

    fix code

--- models/base_model.py
+++ models/base_model.py
@@ -39,7 +39,7 @@ class BaseModel:
         updated_at with the current datetime
         """
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
     def to_dict(self):
         """returns a dictionary containing
         all keys/values of __dict__ of the instance"""
