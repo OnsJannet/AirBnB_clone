adbe51272bdf347231afb8d17a43254ed14e1a63
onsjannet <onsjannet@gmail.com>
2020-11-05 20:20:57 +0100

    pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -40,12 +40,12 @@ class FileStorage:
     def reload(self):
         """reload from JSON"""
         my_class = {"BaseModel": BaseModel, "Amenity": Amenity,
-                "City": City, "Place": Place, "Review": Review,
-                "User": User, "State": State}
+                    "City": City, "Place": Place, "Review": Review,
+                    "User": User, "State": State}
         dic_to_fill = {}
         if(os.stat(self.__file_path).st_size != 0):
             with open(self.__file_path) as json_file:
                 dict_to_fill = json.load(json_file)
                 for key, value in dict_to_fill.items():
-                    self.__objects[key] = my_class[value
-                                              ['__class__']](**value)
+                    self.__objects[key] =\
+                        my_class[value['__class__']](**value)
