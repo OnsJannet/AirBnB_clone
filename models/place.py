98be0a6afd05579719ca0dcbbe085d0bfcc3d9e6
onsjannet <onsjannet@gmail.com>
2020-11-05 13:49:21 +0100

    documentation

--- models/place.py
+++ models/place.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" Place Module"""
 from models.base_model import BaseModel
 
 
@@ -8,10 +9,10 @@ class Place(BaseModel):
     user_id = ""
     name = ""
     description = ""
-    number_rooms = int
-    number_bathrooms = int
-    max_guest = int
-    price_by_night = int
-    latitude = float
-    longitude = float
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
